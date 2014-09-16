95cb7365d59016c9907e5c539a80172ae30bf451
Stefan Junker <root@virthost1>
2014-09-16 05:20:47 +0200

    refactoring and bugfixing

--- docker/build.py
+++ docker/build.py
@@ -1,71 +1,78 @@
 #! ./python2.7_venv/bin/python
 
-from yaml import load, dump
-from bunch import bunchify
+def load_yml():
+    try:
+        from yaml import load, dump
+        from bunch import bunchify
+        datafile = open("build.yml","r")
+        data = load(datafile)
+        return bunchify(data)
+    except ImportError as e:
+        print("Error loading file: " + str(e))
+        os.exit(1)
 
-dataobject = None
+def process_container(container, containerdetail):
+    global_flags = []
+    if hasattr(containerdetail, "use"):
+        for sign, uselist in containerdetail.use.iteritems():
+            for use in uselist:
+                global_flags.append(sign+'use::'+use)
+            
+    packages_install = ""
+    packages_use = {}
+    for package, packagedetail in containerdetail.packages.iteritems():
+        # Generate package atom with version
+        if hasattr(packagedetail, "version"):
+            packagedetail.atom = '='+package+'-'+packagedetail.version
+        else:
+            packagedetail.atom = package
 
-try:
-    datafile = open("build.yml","r")
-    data = load(datafile)
+        # Collect to be installed packages
+        if packagedetail.install == True:
+            packages_install += packagedetail.atom+' '
 
-    dataobject = bunchify(data)
+        # Collect package specific use flags
+        if hasattr(packagedetail, "use"):
+            packages_use[packagedetail.atom] = ""
+            for sign, uselist in packagedetail.use.iteritems():
+                for use in uselist:
+                    packages_use[packagedetail.atom] += sign+'use::'+use+' '
 
-except ImportError as e:
-    print("Error loading file: " + str(e))
+    return {'global_flags': global_flags,
+              'packages_use': packages_use,
+              'packages_install': packages_install}
 
-  
-
-if dataobject != None:
-    
+def parse_objectdata(dataobject):
+    all_vars = {}
+    for container, containerdetail in dataobject.containers.iteritems():
+        try:
+            single_vars = process_container(container,containerdetail)
+            single_vars['metadata'] = dataobject.metadata
+            all_vars[container] = single_vars
+            print("Parsed " + container)
+        except Exception as e:
+            print(e.__class__.__name__+": "+ str(e))
+    return all_vars
+         
+def main():
     import os
     from jinja2 import FileSystemLoader, Environment
-
     # Load jinja
     jinja_loader = FileSystemLoader("templates")
     jinja_env = Environment(loader=jinja_loader,
                             trim_blocks=True,
                             lstrip_blocks=True)
 
-    for container, containerdetail in dataobject.containers.iteritems():
-        print("Processing "+container+"...")
-        global_flags = []
-        if hasattr(containerdetail, "use"):
-            for sign, uselist in containerdetail.use.iteritems():
-                for use in uselist:
-                    global_flags.append(sign+'use::'+use)
-                
-        packages_install = ""
-        packages_use = {}
-        for package, packagedetail in containerdetail.packages.iteritems():
-            # Generate package atom with version
-            if hasattr(packagedetail, "version"):
-                packagedetail.atom = '='+package+'-'+packagedetail.version
-            else:
-                packagedetail.atom = package
 
-            # Collect to be installed packages
-            if packagedetail.install == True:
-                packages_install += packagedetail.atom+' '
+    print("Parsing configuration...")
+    all_vars = parse_objectdata(load_yml())
 
-            # Collect package specific use flags
-            if hasattr(packagedetail, "use"):
-                packages_use[packagedetail.atom] = ""
-                for sign, uselist in packagedetail.use.iteritems():
-                    for use in uselist:
-                        packages_use[packagedetail.atom] += sign+'use::'+use+' '
-
-        thevars = {'metadata': dataobject.metadata,
-                  'container': dataobject.containers.nova,
-                  'global_flags': global_flags,
-                  'packages_use': packages_use,
-                  'packages_install': packages_install}
-
-        # Get the template and render it using `myvars`
+    print("Rendering templates...")
+    for container, single_vars in all_vars.iteritems():
         try:
+            # Get the template and render it 
             tmpl_file = jinja_env.get_template('Dockerfile.'+container+'.jinja')
-            rendered_file = tmpl_file.render(**thevars)
-
+            rendered_file = tmpl_file.render(**single_vars)
             # To write to another file, use `file`
             if not os.path.exists(container):
                 os.makedirs(container)
@@ -77,4 +84,4 @@ if dataobject != None:
         except Exception as e:
             print(e.__class__.__name__+": "+ str(e))
 
-         
+main()
